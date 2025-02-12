faaa47700493d9252b43523e5e09b16c3c992f3b
Imjaad Haider <imjaadhaider@gmail.com>
2025-02-12 14:13:02 +0500

    fixed git global email

--- app/(dashboard)/assets/create/page.tsx
+++ app/(dashboard)/assets/create/page.tsx
@@ -6,7 +6,7 @@ export const CreateAssetPage = async ({}: Props) => {
   return (
     <SessionProvider>
       <div className='p-5'>
-        <h3 className='text-2xl font-semibold'>Add Property</h3>
+        <h3 className='text-2xl font-semibold'>Add Units</h3>
         <p className='text-gray-500'>add new property here</p>
         <CreateAsset />
       </div>
